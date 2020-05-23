eb2f92149e34ff804dfb890eaba0eb88e8324eb9
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-23 23:14:46 +0200

    push intermedia

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+    $.ajax({
+        url: 'https://api.themoviedb.org/3/search/movie',
+        method: 'GET',
+        data: {
+            api_key: '98ca6c1854d7d040d31087b5ed5527b2',
+            query: 'ritorno al futuro'
+        },
+        success: function(response) {
+            console.log(response);
+
+        },
+
+        error: function() {
+            console.log('errore');
+        }
+
+
+    });
+
+
+
+
+
+});
