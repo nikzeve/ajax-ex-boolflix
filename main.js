2443c306b7200cb1574cf8d2c14fd242e46108db
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-05-24 21:28:01 +0200

    ricerca sistemata

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
+    $('#search-icon').click(function() {
+        var control_research = $('#search-movie').val();
+        if (control_research.trim() != '') {
+            var research = control_research
+            console.log(research);
+            $('#search-movie').val('');
+            ricerca_api(research);
+        }
+    });
+
+function ricerca_api(research) {
     $.ajax({
         url: 'https://api.themoviedb.org/3/search/movie',
         method: 'GET',
         data: {
             api_key: '98ca6c1854d7d040d31087b5ed5527b2',
-            query: 'ritorno al futuro'
+            query: research
         },
         success: function(response) {
             console.log(response);
@@ -17,13 +28,6 @@ $(document).ready(function() {
         }
     });
 
-
-    $('#search-icon').click(function() {
-        alert('funziona');
-    });
-
-
-
-
+}
 
 });
